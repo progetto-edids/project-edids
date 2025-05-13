a9d3231bc060ede827934e1b59868b8f20ebcffc
sara-githu <sara.gandini00@gmail.com>
2025-05-13 09:33:52 +0200

    Update UI.java

--- src/main/java/com.nonsensegenerator/UI.java
+++ src/main/java/com.nonsensegenerator/UI.java
@@ -1,5 +1,10 @@
 public class UI {
-
+  System.out.println("Ciao!\nInserisci una frase:");
+  Scanner scanner = new Scanner(System.in);
+  String input = scanner.nextLine();
+ 
+  String[] parole = input.trim().split("\\s+"); // stringa di input tokenizzata
+  
 
   public UI() {
     
