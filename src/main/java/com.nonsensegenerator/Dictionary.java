ae4bd04b7623404c77dbfe39d787f7cea76a0afa
sara-githu <sara.gandini00@gmail.com>
2025-05-13 09:46:09 +0200

    Update Dictionary.java

--- src/main/java/com.nonsensegenerator/Dictionary.java
+++ src/main/java/com.nonsensegenerator/Dictionary.java
@@ -1,6 +1,28 @@
-public class Dictionary {
+import java.util.HashSet;
+import java.util.Set;
 
-  public Dictionary() {
-    
-  }
+public class Dizionario {
+    private Set<String> parole;
+
+    public Dizionario() {
+        this.parole = new HashSet<>();
+        // Dizionario iniziale
+        parole.add("cane");
+        parole.add("correre");
+        parole.add("verde");
+        parole.add("ciao");
+    }
+
+    public boolean contiene(String parola) {
+        return parole.contains(parola.toLowerCase());
+    }
+
+    public void aggiungiParola(String parola) {
+        parole.add(parola.toLowerCase());
+        System.out.println("Parola aggiunta al dizionario: " + parola);
+    }
+
+    public void stampaDizionario() {
+        System.out.println("Dizionario attuale: " + parole);
+    }
 }
