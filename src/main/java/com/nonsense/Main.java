f96e708e2ae7316056c2224c71f69f618a9446e8
alepava04 <alessandro.pavanello04@gmail.com>
2025-05-29 13:55:16 +0200

    Update Main.java

--- src/main/java/com/nonsense/Main.java
+++ src/main/java/com/nonsense/Main.java
@@ -7,26 +7,21 @@ import com.nonsense.Syntax.Syntax;
 import com.nonsense.ToxicityValidator.ToxicityValidator;
 
 
-// Questa è la classe principale del tuo progetto
 public class Main {
 
     public static void main(String[] args) {
         // Inizializza il client Natural Language API usando la classe di utilità
-        // Questo metodo si occupa di caricare le credenziali e creare il client.
         LanguageServiceClient languageServiceClient = NaturalLanguageApiClient.getServiceClient();
 
         // Verifica se il client è stato inizializzato correttamente
         if (languageServiceClient == null) {
             System.err.println("Impossibile inizializzare il client Natural Language API. Controlla i messaggi di errore precedenti.");
-            return; // Termina l'esecuzione se il client non è disponibile
+            return; 
         }
 
         System.out.println("Client Natural Language API inizializzato e pronto per l'uso.");
 
         try {
-            // --- Esempio di utilizzo della Natural Language API in un'altra "classe" o metodo ---
-            // In un progetto reale, questa lomugica potrebbe essere in un metodo separato
-            // o in una classe dedicata all'analisi del testo.
             ToxicityValidator toxicityValidator = new ToxicityValidator(languageServiceClient);
             Syntax syntax = new Syntax(new Dictionary(), languageServiceClient);
              UI ui = new UI(new NonSenseGenerator(), syntax, toxicityValidator);
